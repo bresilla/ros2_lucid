4f3757e6c289f9d8e762b3549abe146def2ac54b
Trim Bresilla <trim.bresilla@gmail.com>
2024-06-23 03:30:26 +0200

    feat: refactor camera configuration and launching
    - Add `fmt` as a required package
    - Update camera configurations in `config/cameras.json`
    - Rename `config/cameras.yaml` to `config/params.yaml`
    - Include new headers in `CameraInitialize.hpp`
    - Modify arguments in `cameras_launch.py`
    - Refactor camera launching in `cameras_launch.py`
    - Implement a new function `named_camera` in `CameraNode.cpp`

--- include/ros2_lucid/CameraInitialize.hpp
+++ include/ros2_lucid/CameraInitialize.hpp
@@ -2,6 +2,11 @@
 
 #include <string>
 #include <unordered_map>
+#include <ament_index_cpp/get_package_share_directory.hpp>
+
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 namespace camset {
     const int GAIN = 1;
@@ -34,4 +39,38 @@ namespace camset {
         {30853686650366, {"11b",   0,   240000,    0 * 80000}}, // directly
         {30853686650497, {"11a",   0,   240000,    0 * 80000}}  // directly
     };
+
+    inline std::map<std::string, uint64_t> by_name = {
+        {"9b", 30853686642969},
+        {"9a", 30853686643065},
+        {"9c", 30853686646563},
+        {"9d", 30853686653149},
+        {"9e", 30853686643056},
+        {"7a", 30853686646554},
+        {"7e", 30853686653140},
+        {"7c", 30853686445113},
+        {"7d", 30853686646528},
+        {"7b", 30853686652294},
+        {"1",  30853686643187},
+        {"4",  30853686650340},
+        {"3",  30853686646397},
+        {"12", 30853686643152},
+        {"10", 30853686646406},
+        {"13", 30853686643161},
+        {"11b",30853686650366},
+        {"11a",30853686650497}
+    };
+
+
+    inline std::map<uint64_t, std::string> get_from_json() {
+        std::string path = ament_index_cpp::get_package_share_directory("ros2_lucid") + "/config/camera.json";
+        std::ifstream ifs(path);
+        json j = json::parse(ifs);
+        std::map<uint64_t, std::string> res;
+        for (auto& [mac, name] : j.items()) {
+
+        //     res[std::stoull(mac)] = name;
+        }
+        return res;
+    }
 }
--- launch/cameras_launch.py
+++ launch/cameras_launch.py
@@ -1,34 +1,50 @@
 import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import json
 
+
+cameras = ["9b",  "9a",  "9c",  "9d",  "9e",  "7a",  "7e",  "7c",  "7d",  "7b",  "1",   "4",   "3",   "12",  "10",   "13",   "11b",  "11a"]
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    camera_discover = Node(
+    camera_info = Node(
         package='genicam',
-        executable='camera_discover',
+        executable='camera_info',
         output="screen",
         emulate_tty=True,
     )
-    camera_node = Node(
-        package='genicam',
-        executable='camera_node',
-        output="screen",
-        emulate_tty=True,
-        arguments=['--managed', 'true'],
-    )
+    ld.add_action(camera_info)
+
+
+    # camera_node = Node(
+    #     package='genicam',
+    #     executable='camera_node',
+    #     output="screen",
+    #     emulate_tty=True,
+    #     arguments=['--managed', 'true'],
+    # )
+    # ld.add_action(camera_node)
+
+    for camera in cameras:
+        camera_node = Node(
+            package='genicam',
+            executable='camera_node',
+            output="screen",
+            emulate_tty=True,
+            arguments=['--managed', camera],
+        )
+        ld.add_action(camera_node)
+
     camera_manager = Node(
         package='genicam',
         executable='camera_manager',
         output="screen",
         emulate_tty=True,
     )
-
-
-    ld.add_action(camera_discover)
-    ld.add_action(camera_node)
     ld.add_action(camera_manager)
+
+
 
     return ld
